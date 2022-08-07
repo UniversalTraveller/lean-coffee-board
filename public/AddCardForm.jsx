f94bbe41262562c4992b4482ebed23088560f254
Andreas Kiener <andreas.kiener@gmail.com>
2022-08-07 22:54:01 +0200

    Add submit button loads the board

--- public/AddCardForm.jsx
+++ public/AddCardForm.jsx
@@ -1,23 +1,49 @@
 import Button from "../public/Button";
 import { Form, Input, Label, InputRow } from "../public/Form";
 
+import { useState } from "react";
 import Link from "next/link";
+import Router, { useRouter } from "next/router";
 
 export default function AddCardForm() {
+  //variables for topic and author with the useState hook
+  const [topic, setTopic] = useState("");
+  const [author, setAuthor] = useState("");
+  const router = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(topic, author);
+    router.push("/");
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <InputRow>
         <Label htmlFor="topic">Topic</Label>
-        <Input type="text" id="topic" />
+        <Input
+          type="text"
+          id="topic"
+          value={topic}
+          onChange={(event) => setTopic(event.target.value)}
+        />
       </InputRow>
       <InputRow>
         <Label htmlFor="author">Author</Label>
-        <Input type="text" id="author" />
+        <Input
+          type="text"
+          id="author"
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        />
       </InputRow>
 
-      <Button>Submit</Button>
+      <Button type="submit">Submit</Button>
+
       <Link href="/">
-        <Button>Cancel</Button>
+        <Button type="button" onClick={(event) => (window.location.href = "/")}>
+          Cancel
+        </Button>
       </Link>
     </Form>
   );
